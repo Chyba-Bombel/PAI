9d60334960a3ca7cf34b4c70a05d97c7a02f393c
Kacper Stolarek <kstolarek@technikumlodz.pl>
2026-09-08 14:17:00 +0200

    Moj pierwszy commit

---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+    // $a = 5;
+    // echo "Hello world: $a";
+    $b = "Hello";
+    // $c = "World";
+    // echo $b.$c;
+    // $tablica = array("Wala", "Japupka", "Dripenko");
+    // // var_dump($tablica);
+    // echo count($tablica);
+    echo substr($b, 0, 2);
+    define("XD", "XD");
+    echo XD;
+?>
