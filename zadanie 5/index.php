d1ddd07af7b18fe7c311d42fd0a5eb7bad99537d
Kacper Stolarek <kstolarek@technikumlodz.pl>
2026-09-15 13:56:00 +0200

    commit

---
+++ zadanie 5/index.php
@@ -0,0 +1,20 @@
+<?php
+    //for
+    for($i = 0; $i < 10; $i++){
+        echo $i;
+    }
+    echo "<br>";
+    //while
+    $i = 0;
+    while($i < 5){
+        echo $i;
+        $i++;
+    }
+    echo "<br>";
+    //do-while
+    $i = 1;
+    do {
+        echo $i;
+        $i++;
+    } while ($i <= 5);
+?>
